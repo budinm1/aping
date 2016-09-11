b671eba67dd1c5bd20e90ecf50a5f6f143480f23
Michal Yupa Budínský <michal@budinsky.net>
2016-09-11 19:29:29 +0200

    first commit

---
+++ aping.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Copyright (C) Michal Budínský <michal@budinsky.net>
+# GIT: https://github.com/budinm1/
+# Licence: WTFPL, grab your copy here: http://www.wtfpl.net/
+
+'''Do pings to list of hosts regulary and make statistics out of it'''
